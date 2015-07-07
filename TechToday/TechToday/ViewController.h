ced8a7febbe5f876951bc8303f9e5399931cd6af
Cai Linfeng <cailinfeng1234@126.com>
2015-07-07 10:01:53 +0800

    initialize.

---
+++ TechToday/TechToday/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TechToday
+//
+//  Created by CaiGavin on 7/7/15.
+//  Copyright (c) 2015 CaiGavin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
