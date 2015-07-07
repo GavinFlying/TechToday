5e092847fddfc96f8395bcf6d8cb8714892e86e0
Cai Linfeng <cailinfeng1234@126.com>
2015-07-07 14:17:55 +0800

    add something

---
+++ TechToday/TechToday/CLFAppDelegate.h
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.h
+//  TechToday
+//
+//  Created by CaiGavin on 15/5/14.
+//  Copyright (c) 2015年 CaiGavin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class JVFloatingDrawerViewController;
+@class JVFloatingDrawerSpringAnimator;
+@class CLFNavigationController;
+
+@interface CLFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) JVFloatingDrawerViewController *drawerViewController;
+@property (strong, nonatomic) JVFloatingDrawerSpringAnimator *drawerAnimator;
+
+@property (strong, nonatomic) UITableViewController *leftDrawerViewController;
+
+@property (strong, nonatomic) CLFNavigationController *centerNavigationController;
+
+@property (assign, nonatomic, getter=isNoImageModeOn) BOOL noImageModeOn;
+
++ (CLFAppDelegate *)globalDelegate;
+
+- (void)toggleLeftDrawer:(id)sender animated:(BOOL)animated;
+
+@end
+
